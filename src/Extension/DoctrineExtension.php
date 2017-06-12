0f6275d7c794e55695797447f616c667b4ff0aa5
allflame <taras.p.gyrnik@gmail.com>
2017-06-13 01:48:23 +0300

    Added Doctrine extension registrator

---
+++ src/Extension/DoctrineExtension.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\ODM\Extension;
+
+use Vainyl\Core\Extension\AbstractExtension;
+
+/**
+ * Class DoctrineExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DoctrineExtension extends AbstractExtension
+{
+}
