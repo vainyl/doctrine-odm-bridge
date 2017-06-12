fa7cb687c58764e0cf10e444ed8b55a32d57f294
allflame <taras.p.gyrnik@gmail.com>
2017-06-13 01:46:28 +0300

    Added Exception classes

---
+++ src/Exception/LevelIntegrityException.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\ODM\Exception;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+
+class LevelIntegrityException extends AbstractDocumentManagerException
+{
+    private $level;
+
+    /**
+     * LevelIntegrityException constructor.
+     * @param DocumentManager $documentManager
+     * @param int $level
+     */
+    public function __construct(DocumentManager $documentManager, int $level)
+    {
+        $this->level = $level;
+        parent::__construct($documentManager, sprintf('Level integrity check failed for level %d', $level));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        return array_merge(['level' => $this->level], parent::toArray());
+    }
+}
