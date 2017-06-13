1b64b3a79ac633f0826b65016f72b091b256766d
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-13 15:58:14 +0300

    Added database, operation factory parsing

--- src/Extension/DoctrineODMExtension.php
+++ src/Extension/DoctrineODMExtension.php
@@ -12,6 +12,10 @@ declare(strict_types=1);
 
 namespace Vainyl\Doctrine\ODM\Extension;
 
+use Symfony\Component\DependencyInjection\Alias;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Vainyl\Core\Application\EnvironmentInterface;
+use Vainyl\Core\Exception\MissingRequiredServiceException;
 use Vainyl\Core\Extension\AbstractExtension;
 
 /**
@@ -21,4 +25,32 @@ use Vainyl\Core\Extension\AbstractExtension;
  */
 class DoctrineODMExtension extends AbstractExtension
 {
+    /**
+     * @inheritDoc
+     */
+    public function load(
+        array $configs,
+        ContainerBuilder $container,
+        EnvironmentInterface $environment = null
+    ): AbstractExtension {
+        if (false === $container->hasDefinition('database.document')) {
+            throw new MissingRequiredServiceException($container, 'database.document');
+        }
+
+        $definition = $container->getDefinition('database.document');
+        if ($definition->isSynthetic()) {
+            $container->set('database.document', new Alias('database.document.doctrine'));
+        }
+
+        if (false === $container->hasDefinition('document.operation.factory')) {
+            throw new MissingRequiredServiceException($container, 'document.operation.factory');
+        }
+
+        $definition = $container->getDefinition('document.operation.factory.doctrine');
+        if ($definition->isSynthetic()) {
+            $container->set('document.operation.factory', new Alias('document.operation.factory.doctrine'));
+        }
+
+        return parent::load($configs, $container, $environment);
+    }
 }
