c727da23ad4c3d75c2aedc7ba3df65f70b9d7cb0
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-13 16:07:06 +0300

    Added database, operation factory parsing

---
+++ src/Extension/DoctrineDatabaseCompilerPass.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine-orm-bridge
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\ODM\Extension;
+
+use Symfony\Component\DependencyInjection\Alias;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Vainyl\Core\Exception\MissingRequiredServiceException;
+
+/**
+ * Class DoctrineDatabaseCompilerPass
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DoctrineDatabaseCompilerPass implements CompilerPassInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (false === $container->hasDefinition('database.document')) {
+            throw new MissingRequiredServiceException($container, 'database.document');
+        }
+
+        $definition = $container->getDefinition('database.document');
+        if ($definition->isSynthetic()) {
+            $container->set('database.document', new Alias('database.document.doctrine'));
+        }
+
+        return $this;
+    }
+}
--- src/Extension/DoctrineODMExtension.php
+++ src/Extension/DoctrineODMExtension.php
@@ -12,10 +12,8 @@ declare(strict_types=1);
 
 namespace Vainyl\Doctrine\ODM\Extension;
 
-use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Vainyl\Core\Application\EnvironmentInterface;
-use Vainyl\Core\Exception\MissingRequiredServiceException;
 use Vainyl\Core\Extension\AbstractExtension;
 
 /**
@@ -33,23 +31,8 @@ class DoctrineODMExtension extends AbstractExtension
         ContainerBuilder $container,
         EnvironmentInterface $environment = null
     ): AbstractExtension {
-        if (false === $container->hasDefinition('database.document')) {
-            throw new MissingRequiredServiceException($container, 'database.document');
-        }
-
-        $definition = $container->getDefinition('database.document');
-        if ($definition->isSynthetic()) {
-            $container->set('database.document', new Alias('database.document.doctrine'));
-        }
-
-        if (false === $container->hasDefinition('document.operation.factory')) {
-            throw new MissingRequiredServiceException($container, 'document.operation.factory');
-        }
-
-        $definition = $container->getDefinition('document.operation.factory.doctrine');
-        if ($definition->isSynthetic()) {
-            $container->set('document.operation.factory', new Alias('document.operation.factory.doctrine'));
-        }
+        $container->addCompilerPass(new DoctrineDatabaseCompilerPass())
+                  ->addCompilerPass(new DoctrineFactoryCompilerPass());
 
         return parent::load($configs, $container, $environment);
     }
