5bbc3f86adf3e0a89b95e1a0224e2fe56dd58d97
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-16 16:18:13 +0300

    Added DcotrineAnnotationBootstrapper

---
+++ src/Bootstrapper/DoctrineAnnotationBootstrapper.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine-orm-bridge
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\ODM\Bootstrapper;
+
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+use Vainyl\Core\AbstractIdentifiable;
+use Vainyl\Core\Application\ApplicationInterface;
+use Vainyl\Core\Application\BootstrapperInterface;
+
+/**
+ * Class DoctrineAnnotationBootstrapper
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DoctrineAnnotationBootstrapper extends AbstractIdentifiable implements BootstrapperInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        return 'doctrine.odm_annotation';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(ApplicationInterface $application): BootstrapperInterface
+    {
+        AnnotationDriver::registerAnnotationClasses();
+
+        return $this;
+    }
+}
