0569ea0aef5330fcadc954a14c054d6c09514b05
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-16 16:18:25 +0300

    Added dummy TypeBootstrapper

---
+++ src/Bootstrapper/DoctrineTypeBootstrapper.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine-orm-bridge
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\ODM\Bootstrapper;
+
+use Vainyl\Core\AbstractIdentifiable;
+use Vainyl\Core\Application\ApplicationInterface;
+use Vainyl\Core\Application\BootstrapperInterface;
+use Vainyl\Time\Factory\TimeFactoryInterface;
+
+/**
+ * Class DoctrineTypeBootstrapper
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DoctrineTypeBootstrapper extends AbstractIdentifiable implements BootstrapperInterface
+{
+    private $timeFactory;
+
+    /**
+     * DoctrineTypeBootstrapper constructor.
+     *
+     * @param TimeFactoryInterface $timeFactory
+     */
+    public function __construct(TimeFactoryInterface $timeFactory)
+    {
+        $this->timeFactory = $timeFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        return 'doctrine.odm_type';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(ApplicationInterface $application): BootstrapperInterface
+    {
+        return $this;
+    }
+}
