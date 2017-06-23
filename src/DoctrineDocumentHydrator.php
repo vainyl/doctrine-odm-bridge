e4c0ddf61d8f605a8a1862b48e50cfb734884c36
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-23 14:22:33 +0300

    Added missing reference handling

--- src/DoctrineDocumentHydrator.php
+++ src/DoctrineDocumentHydrator.php
@@ -14,15 +14,18 @@ namespace Vainyl\Doctrine\ODM;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Persistence\ManagerRegistry as DoctrineRegistryInterface;
-use Doctrine\Common\Persistence\ObjectRepository as DoctrineRepositoryInterface;
 use Doctrine\Common\Persistence\Mapping\MappingException;
+use Doctrine\Common\Persistence\ObjectManager as DoctrineManagerInterface;
+use Doctrine\Common\Persistence\ObjectRepository as DoctrineRepositoryInterface;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
 use Doctrine\ODM\MongoDB\Mapping\ClassMetadataFactory;
+use Doctrine\ODM\MongoDB\PersistentCollection;
 use Doctrine\ODM\MongoDB\Types\Type;
 use Vainyl\Core\ArrayInterface;
 use Vainyl\Core\Hydrator\AbstractHydrator;
 use Vainyl\Doctrine\ODM\Exception\MissingDiscriminatorColumnException;
 use Vainyl\Doctrine\ODM\Exception\UnknownDiscriminatorValueException;
+use Vainyl\Doctrine\ODM\Exception\UnknownReferenceEntityException;
 use Vainyl\Document\DocumentInterface;
 
 /**
@@ -95,13 +98,34 @@ class DoctrineDocumentHydrator extends AbstractHydrator
     }
 
     /**
+     * @return DoctrineManagerInterface
+     */
+    public function getDocumentManager(): DoctrineManagerInterface
+    {
+        return $this->doctrineRegistry->getManager('document');
+    }
+
+    /**
      * @param string $className
      *
      * @return DoctrineRepositoryInterface
      */
     public function getRepository(string $className): DoctrineRepositoryInterface
     {
-        return $this->doctrineRegistry->getRepository($className, 'document');
+        return $this->getDocumentManager()->getRepository($className);
+    }
+
+    /**
+     * @param array $mapping
+     *
+     * @return PersistentCollection
+     */
+    public function getPersistentCollection(array $mapping): PersistentCollection
+    {
+        return $this->getDocumentManager()
+                    ->getConfiguration()
+                    ->getPersistentCollectionFactory()
+                    ->create($this->getDocumentManager(), $mapping);
     }
 
     /**
@@ -118,43 +142,49 @@ class DoctrineDocumentHydrator extends AbstractHydrator
         $document = $classMetadata->newInstance();
 
         foreach ($documentData as $field => $value) {
+            $reflectionField = $processedValue = null;
             switch (true) {
                 case array_key_exists($field, $classMetadata->associationMappings):
                     $associationMapping = $classMetadata->associationMappings[$field];
                     $referenceEntity = $associationMapping['targetDocument'];
                     switch ($associationMapping['association']) {
                         case ClassMetadata::REFERENCE_ONE:
-                            $classMetadata->reflFields[$associationMapping['fieldName']]
-                                ->setValue(
-                                    $document,
-                                    $this->getRepository($referenceEntity)->find($value)
-                                );
+                            if (null === ($processedValue = $this->getRepository($referenceEntity)->find($value))) {
+                                throw new UnknownReferenceEntityException($this, $referenceEntity, $value);
+                            }
 
+                            $reflectionField = $classMetadata->reflFields[$associationMapping['fieldName']];
                             break;
                         case ClassMetadata::REFERENCE_MANY:
-                            $collection = new ArrayCollection();
+                            $processedValue = new ArrayCollection();
                             $repository = $this->getRepository($referenceEntity);
                             foreach ($value as $referenceData) {
-                                $collection->add($repository->find($referenceData));
+                                if (null === ($reference = $repository->find($referenceData))) {
+                                    throw new UnknownReferenceEntityException($this, $referenceEntity, $referenceData);
+                                }
+                                $processedValue->add($reference);
                             }
-                            $classMetadata->reflFields[$associationMapping['fieldName']]
-                                ->setValue(
-                                    $document,
-                                    $collection
-                                );
-
+                            $reflectionField = $classMetadata->reflFields[$associationMapping['fieldName']];
                             break;
                         case ClassMetadata::EMBED_ONE:
+                            $reflectionField = $classMetadata->reflFields[$associationMapping['fieldName']];
+                            $processedValue = 1;
                             break;
                         case ClassMetadata::EMBED_MANY:
+                            $reflectionField = $classMetadata->reflFields[$associationMapping['fieldName']];
+                            $processedValue = 2;
                             break;
                     }
                     break;
                 case array_key_exists($field, $classMetadata->fieldMappings):
                     $fieldMapping = $classMetadata->fieldMappings[$field];
-                    $classMetadata->reflFields[$fieldMapping['fieldName']]
-                        ->setValue($document, Type::getType($fieldMapping['type'])->convertToPHPValue($value));
+                    $processedValue = Type::getType($fieldMapping['type'])->convertToPHPValue($value);
+                    $reflectionField = $classMetadata->reflFields[$fieldMapping['fieldName']];
+
                     break;
+            }
+            if (null !== $reflectionField) {
+                $reflectionField->setValue($document, $processedValue);
             }
         }
 
