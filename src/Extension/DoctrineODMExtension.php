ca86dc9f0f007576c36cfa35fb3962a41566cc88
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-07-31 11:01:36 +0300

    Refactored settings parsing

--- src/Extension/DoctrineODMExtension.php
+++ src/Extension/DoctrineODMExtension.php
@@ -12,10 +12,13 @@ declare(strict_types=1);
 
 namespace Vainyl\Doctrine\ODM\Extension;
 
+use Documents\Functional\Reference;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Vainyl\Core\Exception\MissingRequiredServiceException;
 use Vainyl\Core\Extension\AbstractExtension;
 use Vainyl\Core\Extension\AbstractFrameworkExtension;
+use Vainyl\Doctrine\ODM\Factory\DoctrineODMSettings;
 
 /**
  * Class DoctrineODMExtension
@@ -37,15 +40,23 @@ class DoctrineODMExtension extends AbstractFrameworkExtension
 
         $configuration = new DoctrineODMConfiguration();
         $odmConfig = $this->processConfiguration($configuration, $configs);
-
+        $settingsDefinition = (new Definition())
+            ->setClass(DoctrineODMSettings::class)
+            ->setArguments(
+                [
+                    new Reference('doctrine.settings'),
+                    $odmConfig['database'],
+                    $odmConfig['config'],
+                    $odmConfig['file'],
+                    $odmConfig['extension'],
+                    $odmConfig['tmp_dir'],
+                    $odmConfig['proxy'],
+                    $odmConfig['hydrator'],
+                ]
+            );
+        $container->setDefinition('doctrine.settings.odm', $settingsDefinition);
         $definition = $container->getDefinition('doctrine.configuration.odm');
-        $definition->replaceArgument(2, $odmConfig['database']);
-        $definition->replaceArgument(3, $odmConfig['config']);
-        $definition->replaceArgument(4, $odmConfig['file']);
-        $definition->replaceArgument(5, $odmConfig['extension']);
-        $definition->replaceArgument(6, $odmConfig['tmp_dir']);
-        $definition->replaceArgument(7, $odmConfig['proxy']);
-        $definition->replaceArgument(8, $odmConfig['hydrator']);
+        $definition->replaceArgument(1, new Reference('doctrine.settings.odm'));
 
         return $this;
     }
