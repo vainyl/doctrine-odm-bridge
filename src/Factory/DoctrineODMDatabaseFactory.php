c1b94bfcc0c3175b96699848026c6d8dec4753d4
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-08-08 13:59:05 +0300

    Fixed phpdoc

--- src/Factory/DoctrineODMDatabaseFactory.php
+++ src/Factory/DoctrineODMDatabaseFactory.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 7
  *
- * @package   Doctrine-ODM-Bridge-common-bridge
+ * @package   Doctrine-ODM-Bridge
  * @license   https://opensource.org/licenses/MIT MIT License
  * @link      https://vainyl.com
  */
